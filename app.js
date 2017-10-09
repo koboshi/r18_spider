226c6aa0496f7349a51cd0c797897520bbb0ae17
koboshi <koboshi@qq.com>
2017-10-09 22:48:21 +0800

    developing at 2017-10-09

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var util = require('util');
+var cheerio = require('cheerio');
 var lib = require('./lib');
 
 module.exports = App;
@@ -13,8 +14,8 @@ util.inherits(App, Object);
 
 /**
  * 爬取入口
- * 成人影片 http://www.r18.com/videos/vod/movies/list/pagesize=30/price=all/sort=new/type=all/page=1/
- * 素人影片 http://www.r18.com/videos/vod/amateur/list/pagesize=30/price=all/sort=new/type=all/page=1/
+ * 成人影片 http://www.r18.com/videos/vod/movies/list/pagesize=120/price=all/sort=new/type=all/page=1/
+ * 素人影片 http://www.r18.com/videos/vod/amateur/list/pagesize=120/price=all/sort=new/type=all/page=1/
  */
 App.prototype.start = function() {
     if (this.type == 'movies') {
@@ -32,19 +33,46 @@ App.prototype.start = function() {
  * 成人影片抓取
  */
 App.prototype.movies = function() {
-    console.log('grabing');
-    lib.http.r18_get('/videos/vod/movies/list/pagesize=30/price=all/sort=new/type=all/page=1/',
-    function(err, statusCode, content) {
-        if (err) {
-            throw err;
-        }
-        if (statusCode == 200) {
-            console.log(content);
-        }else {
-            console.warn('request_error:/videos/vod/movies/list/pagesize=30/price=all/sort=new/type=all/page=1/', statusCode);
-        }
-
-    });
+    console.log('start grab', this.type, this.index, this.total);
+    var page = 1;
+    while (1) {
+        var path = '/videos/vod/movies/list/pagesize=30/price=all/sort=new/type=all/page=' + page + '/';
+        lib.http.r18_get(path, function(err, statusCode, content) {
+            if (err) {
+                console.log('error:', path, err.message);//请求异常
+                return;
+            }
+            if (statusCode != 200) {
+                console.log('request:', path, statusCode);//请求失败
+                return;
+            }
+            //解析html内容
+            var infoList = lib.r18.list(content);
+            //爬取详情信息
+            for (i in infoList) {
+                var info = infoList[i];
+                var title = info.title;
+                var cover = info.cover;
+                var detailPath = info.path;
+                lib.http.r18_get(detailPath, function(err, statusCode, content) {
+                    if (err) {
+                        console.log('error:', path, err.message);//请求异常
+                        return;
+                    }
+                    if (statusCode != 200) {
+                        console.log('request:', path, statusCode);//请求失败
+                        return;
+                    }
+                    //解析html内容
+                    var detailInfo = lib.r18.detail(content);
+                    console.log(detailInfo);
+                    //TODO 写入数据库
+                });
+                break;
+            }
+        });
+        break;
+    }
 }
 
 /**
