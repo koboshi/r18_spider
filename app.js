b6cc60b6adb75fc47c57a37529861f6b55cc4b8e
koboshi <koboshi@qq.com>
2017-10-11 00:26:21 +0800

    developing at 2017-10-11

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var util = require('util');
 var cheerio = require('cheerio');
+var async = require('async');
 var lib = require('./lib');
 
 module.exports = App;
@@ -35,9 +36,11 @@ App.prototype.start = function() {
 App.prototype.movies = function() {
     console.log('start grab', this.type, this.index, this.total);
     var page = 1;
+    //TODO 获取最大页码
     while (1) {
         var path = '/videos/vod/movies/list/pagesize=30/price=all/sort=new/type=all/page=' + page + '/';
         lib.http.r18_get(path, function(err, statusCode, content) {
+            console.log('start:', path);//请求异常
             if (err) {
                 console.log('error:', path, err.message);//请求异常
                 return;
@@ -48,28 +51,40 @@ App.prototype.movies = function() {
             }
             //解析html内容
             var infoList = lib.r18.list(content);
+            var detailTask = [];
             //爬取详情信息
             for (i in infoList) {
-                var info = infoList[i];
-                var title = info.title;
-                var cover = info.cover;
-                var detailPath = info.path;
-                lib.http.r18_get(detailPath, function(err, statusCode, content) {
-                    if (err) {
-                        console.log('error:', path, err.message);//请求异常
-                        return;
-                    }
-                    if (statusCode != 200) {
-                        console.log('request:', path, statusCode);//请求失败
-                        return;
-                    }
-                    //解析html内容
-                    var detailInfo = lib.r18.detail(content);
-                    console.log(detailInfo);
-                    //TODO 写入数据库
-                });
-                break;
+                (function(arg) {
+                    var title = arg.title;
+                    var cover = arg.cover;
+                    var detailPath = arg.path;
+                    detailTask.push(function(callback) {
+                        lib.http.r18_get(detailPath, function(err, statusCode, content) {
+                            if (err) {
+                                console.log('error:', path, err.message);//请求异常
+                                return;
+                            }
+                            if (statusCode != 200) {
+                                console.log('request:', path, statusCode);//请求失败
+                                return;
+                            }
+                            //解析html内容
+                            var detailInfo = lib.r18.detail(content);
+                            //console.log(detailInfo);
+                            //抓取完毕，停止5s
+                            setTimeout(function() {
+                                callback(null, detailInfo.title);//调用回调
+                            }, 5000);
+                            //TODO 写入数据库
+                        });
+                    });
+                })(infoList[i]);
             }
+            //按次序执行爬取
+            async.series(detailTask, function(err, result) {
+                console.log(result);
+                console.log('done:', path);
+            });
         });
         break;
     }
